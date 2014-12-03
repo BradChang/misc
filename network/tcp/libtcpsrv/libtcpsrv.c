43ad8d95cb0e0b763c831cb302550b84ae3b80e4
Troy D. Hanson <tdh@tkhanson.net>
2014-12-03 17:45:13 -0500

    pthread_timedjoin_np

--- network/tcp/libtcpsrv/libtcpsrv.c
+++ network/tcp/libtcpsrv/libtcpsrv.c
@@ -390,9 +390,10 @@ int tcpsrv_run(void *_t) {
 
 void tcpsrv_fini(void *_t) {
   int n,rc;
+  struct timespec max_wait = {.tv_sec=1,.tv_nsec=0};
   tcpsrv_t *t = (tcpsrv_t*)_t;
   for(n=0; n < t->p.nthread; n++) { // wait for thread term 
-    rc=pthread_join(t->th[n],NULL);
+    rc=pthread_timedjoin_np(t->th[n],NULL,max_wait);
     if (rc == -1) fprintf(stderr,"pthread_join %d: %s\n",n,strerror(errno));
     else if (t->p.verbose) fprintf(stderr,"thread %d exited\n",n);
   }
