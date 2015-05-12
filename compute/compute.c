7a3aa1d5d3c68dc4469080d5f77b2f0ff836209b
Troy D. Hanson <tdh@tkhanson.net>
2015-05-12 10:51:08 -0400

    sample

--- compute/compute.c
+++ compute/compute.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -10,8 +12,13 @@
 #include <unistd.h>
 
 /*
- *
+ * template of a multi-process (forking) compute program
  */
+
+/* TODO
+ * signal handling
+ */
+
 
 #define NUM_CONFIGS_ALL (1UL << 27U)
 
@@ -24,32 +31,51 @@ struct {
   int verbose;
   char *file; /* output file */
   int fd;     /* of file */
-  int index;  /* TODO */
   int workers;
+  int worker_idx; /* 0-workers */
   /* internals */
   char *buf;     /* mmap'd output file */
   size_t buf_sz; /* size of above (bytes) */
   time_t start;  
   time_t end;  
+  int sts_pipe[2]; /* [0] = read end, [1]=write end */
 } CF = {
   .buf_sz = NUM_CONFIGS_ALL / 8, /* this many bytes to hold that bit array */
   .fd = -1,
-  .file = "3x3x3.dat",
+  .workers = 1,
+  .file = "output.dat",
+};
+
+struct worker_status {
+  int idx;
+  int status;
 };
 
 void usage(char *prog) {
-  fprintf(stderr, "usage: %s [-v] [-f <file>] [-i <i/j>]\n", prog);
-  fprintf(stderr,"        i/j = index of this worker, of j workers\n");
+  fprintf(stderr, "usage: %s [-v] [-f <file>] [-j <#workers>]\n", prog);
   exit(-1);
 }
 
-void compute() {
-  long i;
-  char *c = CF.buf;
-  for(i=0; i<NUM_CONFIGS_ALL; i++) {
-    /* work goes here */
-    BIT_SET(c,i);
-  }
+/* executed in each worker */
+int work() {
+  struct worker_status ps;
+  ps.idx = CF.worker_idx;
+  ps.status = 0;
+
+  prctl(PR_SET_PDEATHSIG, SIGHUP); // get signal on parent exit
+  close(CF.sts_pipe[0]); // close read end
+
+  do {
+    sleep(10); // replace this with real work
+    ps.status += 10;
+    if (write(CF.sts_pipe[1], &ps, sizeof(ps)) < 0) {
+      fprintf(stderr,"worker %d: write %s\n", ps.idx, strerror(errno));
+    }
+  } while (ps.status < 100);
+
+  /* close pipe. when all workers have closed it, parent gets eof */
+  close(CF.sts_pipe[1]);
+  return 0;
 }
 
 void cleanup_mapping() {
@@ -89,22 +115,42 @@ int map() {
 }
 
 int main(int argc, char *argv[]) {
-  int opt,verbose=0;
-  while ( (opt = getopt(argc, argv, "v+f:hi:j:")) != -1) {
+  struct worker_status ps;
+  int opt,verbose=0,rc,i;
+  pid_t pid;
+
+  while ( (opt = getopt(argc, argv, "v+f:hj:")) != -1) {
     switch (opt) {
       case 'v': CF.verbose++; break;
       case 'f': CF.file=strdup(optarg); break;
-      case 'i': CF.index=atoi(optarg); break;
       case 'j': CF.workers=atoi(optarg); break;
       case 'h': default: usage(argv[0]); break;
     }
   }
-  if (map() < 0) goto done;
 
   time(&CF.start);
   fprintf(stderr,"starting at %s", asctime(localtime(&CF.start)));
 
-  compute();
+  if (pipe(CF.sts_pipe) < 0) goto done;
+  if (map() < 0) goto done;
+
+  while (CF.worker_idx < CF.workers) {
+    pid = fork();
+    if (pid < 0) {fprintf(stderr,"fork: %s\n", strerror(errno)); goto done;}
+    if (pid > 0) fprintf(stderr,"worker %d: pid %d\n", CF.worker_idx, pid);
+    if (pid== 0) return work();
+    CF.worker_idx++;
+  }
+
+  close(CF.sts_pipe[1]); // parent close write end of pipe
+
+  do {
+    rc = read(CF.sts_pipe[0], &ps, sizeof(ps));
+    if (rc == sizeof(ps)) fprintf(stderr,"worker %d: %d\n", ps.idx, ps.status);
+    if (rc <= 0) fprintf(stderr,"read: %s\n", (rc<0) ? strerror(errno) : "eof");
+  } while (rc > 0);
+
+  for(i=0; i < CF.workers; i++) { wait(NULL); fprintf(stderr,"worker exited\n"); }
 
   time(&CF.end);
   fprintf(stderr,"ending at %s", asctime(localtime(&CF.end)));
