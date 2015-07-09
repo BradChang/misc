b6075f1cd09401dda3ed23698ae3d2bf8db465b8
Troy D. Hanson <tdh@tkhanson.net>
2015-07-09 10:48:05 -0400

    wip

--- minecraft/schem.c
+++ minecraft/schem.c
@@ -3,7 +3,17 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include "utvector.h"
 #include "tpl.h"
+
+typedef struct {
+  int tag;  /* TAG_Compound or TAG_List */
+  struct {  /* for TAG_List, items are: */
+    int tag;
+    int count;
+  } list;
+} nbt_parse;
+const UT_vector_mm nbt_parse_mm = {.sz=sizeof(nbt_parse)};
 
 #define TAG_End        0 
 #define TAG_Byte       1 
@@ -16,6 +26,16 @@
 #define TAG_String     8 /* length (TAG_Short)-prefixed UTF8 */
 #define TAG_List       9 /* TAG_Byte tagID, TAG_Int length, array */
 #define TAG_Compound  10 /* list of named tags unti TAG_End */
+#define TAG_MAX TAG_Compound
+
+size_t tag_sizes[TAG_MAX+1] = { /* the payload sizes for the simple tags */
+  [TAG_Byte] =  sizeof(int8_t),
+  [TAG_Short] = sizeof(int16_t),
+  [TAG_Int] =   sizeof(int32_t),
+  [TAG_Long] =  sizeof(int64_t),
+  [TAG_Float]=  sizeof(int32_t),
+  [TAG_Double]= sizeof(int64_t),
+};
 
 /* here is a sample of the start of a .schematic file 
 *  see Minecraft wiki for NBT file format and Schematic file format 
@@ -54,7 +74,14 @@ int make_schem_tpl(char *in, size_t ilen, char **out, size_t *olen) {
   int rc = -1;
   tpl_node *tn=NULL;
   char *p = in;
-  size_t len = ilen;
+  size_t len = ilen, tag_size;
+  UT_vector *nbt_stack;
+  char tag_type, *tag_name, *tag_payload;
+  uint16_t name_len, str_len;
+  int32_t array_len;
+  nbt_parse np, *top;
+
+  nbt_stack = utvector_new(&nbt_parse_mm);
 
   int16_t width, height, length, x, y, z, l;
   char id;
@@ -66,6 +93,66 @@ int make_schem_tpl(char *in, size_t ilen, char **out, size_t *olen) {
   if (tn==NULL) goto done;
 
   /* parse the .schematic NBT. we only want dimensions and block ids */
+  while(len) {
+    tag_type = *p;
+
+    if (tag_type == TAG_End) { /* special case */
+      top = (nbt_parse*)utvector_tail(nbt_stack);
+      if (top->tag != TAG_Compound) goto done;
+      utvector_pop(nbt_stack);
+      len--;
+      continue;
+    }
+
+    /* other tags all have a length-prefixed name */
+    if (len < sizeof(uint16_t)) goto done;
+    memcpy(&name_len, p, sizeof(uint16_t)); name_len = ntohs(name_len);
+    len -= sizeof(uint16_t); p += sizeof(uint16_t);
+    if (len < name_len) goto done;
+    tag_name = p;
+    len -= name_len; p += name_len;
+    tag_payload = p;
+    //notate(tag_type, tag_name, name_len, tag_payload, len);
+
+    switch(tag_type) {
+      case TAG_Byte:
+      case TAG_Short:
+      case TAG_Int:
+      case TAG_Long:
+      case TAG_Float:
+      case TAG_Double:
+        tag_size = tag_sizes[tag_type]; assert(tag_size > 0);
+        if (len < tag_size) goto done;
+        len -= tag_size; p += tag_size;
+        break;
+      case TAG_Byte_Array: 
+        if (len < sizeof(array_len)) goto done;
+        memcpy(&array_len, p, sizeof(array_len)); array_len = ntohl(array_len);
+        len -= sizeof(array_len); p += sizeof(array_len);
+        tag_size = array_len;
+        if (len < tag_size) goto done;
+        len -= tag_size; p += tag_size;
+        break;
+      case TAG_String: 
+        if (len < sizeof(str_len)) goto done;
+        memcpy(&str_len, p, sizeof(str_len)); str_len = ntohs(str_len);
+        len -= sizeof(str_len); p += sizeof(str_len);
+        tag_size = str_len;
+        if (len < tag_size) goto done;
+        len -= tag_size; p += tag_size;
+        break;
+      case TAG_List: 
+        break;
+      case TAG_Compound: 
+        break;
+      case TAG_End: 
+        assert(0); 
+        break;
+      default: 
+        goto done; 
+        break;
+    }
+  }
 
   /* first we want TAG_Compound, with a payload of Tag_String "Schematic" */
   if (len < 1 + 2 + 9) goto done;
@@ -73,6 +160,8 @@ int make_schem_tpl(char *in, size_t ilen, char **out, size_t *olen) {
   memcpy(&l, p, sizeof(l)); l = ntohs(l);
   if (l != 9) goto done; p += sizeof(l);
   if (memcmp(p,"Schematic",9)) goto done;
+  np.tag = TAG_Compound;
+  utvector_push(nbt_stack, &np);
 
   rc = tpl_dump(tn, TPL_MEM, out, olen);
   if (rc < 0) goto done;
@@ -81,6 +170,7 @@ int make_schem_tpl(char *in, size_t ilen, char **out, size_t *olen) {
 
  done:
   if (tn) tpl_free(tn);
+  utvector_free(nbt_stack);
   return rc;
 }
 
