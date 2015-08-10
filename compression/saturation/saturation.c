6ede940007a09d8cebf6c78d45c8466009860774
Troy D. Hanson <tdh@tkhanson.net>
2015-08-10 12:26:10 -0400

    cleanup

--- compression/saturation/saturation.c
+++ compression/saturation/saturation.c
@@ -11,8 +11,8 @@
 
 /* standard bit vector macros */
 #define BIT_TEST(c,i)  ((c[(i)/8] &   (1 << ((i) % 8))) ? 1 : 0)
-#define BIT_SET(c,i)   (c[(i)/8] |=  (1 << ((i) % 8)))
-#define BIT_CLEAR(c,i) (c[(i)/8] &= ~(1 << ((i) % 8)))
+#define BIT_SET(c,i)    (c[(i)/8] |=  (1 << ((i) % 8)))
+#define BIT_CLEAR(c,i)  (c[(i)/8] &= ~(1 << ((i) % 8)))
 
 /* 
  * 
